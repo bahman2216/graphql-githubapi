0bc7cf921add03b188478242dc777246b3a0f105
ghazavi <mohammad.ghazavi@gmail.com>
2022-11-26 18:10:35 +0330

    add DoSearch

--- src/components/DoSearch.js
+++ src/components/DoSearch.js
@@ -3,17 +3,27 @@ import { gql,useQuery} from "@apollo/client";
 
 export default function DoSearch() {
   const GET_REPOS = gql`
-  query
-          repositories {
-            nodes {
-              name
-            }
-         
+  query search($query: String!, $type: SearchType!, $numOfResults: Int!) {
+    search(type: $type, query: $query, first: $numOfResults) {
+      nodes {
+        ... on Repository {
+          name
+          nameWithOwner
+          description
+          
+      
+        }
         }
       }
+  }
   `;
 
-  const { loading, error, data } = useQuery(GET_REPOS);
+  const { loading, error, data } = useQuery(GET_REPOS, {variables: {
+    "query": "facebook/react",
+    "type": "REPOSITORY",
+    "numOfResults": 10
+  }
+   });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
